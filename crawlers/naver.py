089bfa362bc47640a766ac276384c4c652d89d91
Gyupil <mamonde1015@gmail.com>
2025-11-07 03:12:51 +0900

    Add more crawling function for Naver

--- crawlers/naver.py
+++ crawlers/naver.py
@@ -54,7 +54,59 @@ def get_naver_datalab_interest(keyword):
     return {'naver_datalab_daily_ratio': yesterday_ratio}
 
 
+def get_naver_mentions_24h(keyword):
+    NAVER_API_URL_BLOG = "https://openapi.naver.com/v1/search/blog.json"
+    NAVER_API_URL_CAFE = "https://openapi.naver.com/v1/search/cafearticle.json"
+    HEADERS = {
+        "X-Naver-Client-Id": NAVER_CLIENT_ID,
+        "X-Naver-Client-Secret": NAVER_CLIENT_SECRET,
+    }
+
+    api_urls = [NAVER_API_URL_BLOG, NAVER_API_URL_CAFE]
+    total_count_24h = 0
+
+    params = {
+        'query': keyword,
+        'display': 100,
+        'sort': 'date'
+    }
+
+    for api_url in api_urls:
+        api_name = "블로그" if "blog" in api_url else "카페"
+
+        response = requests.get(api_url, headers=HEADERS, params=params)
+        response.raise_for_status()
+
+        data = response.json()
+        items = data.get('items', [])
+
+        if not items:
+            print(f"[NaverCrawler] '{keyword}' {api_name} 검색 결과 없음")
+            continue
+
+        now = datetime.now()
+        yesterday = now - timedelta(days=1)
+
+        for item in items:
+            post_time = None
+            if 'postdate' in item:
+                date_str = item['postdate']
+                post_time = datetime.strptime(date_str, "%Y%m%d")
+            elif 'pubDate' in item:
+                date_str = item['pubDate']
+                post_time = datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S %z")
+                post_time = post_time.replace(tzinfo=None)
+
+            if post_time and post_time >= yesterday:
+                total_count_24h += 1
+            elif post_time and post_time < yesterday:
+                break
+
+    return {'naver_mentions_24h': total_count_24h}
+
 if __name__ == '__main__':
     TEST_KEYWORD = "한로로"  # 테스트할 키워드
     stats = get_naver_datalab_interest(TEST_KEYWORD)
-    print(f"--- Naver DataLab '{TEST_KEYWORD}' 최종 결과 ---", stats)
+    stat2 = get_naver_mentions_24h(TEST_KEYWORD)
+    print(stats)
+    print(stat2)
